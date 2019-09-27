b1b031e45732ef671c9f23c44afe293a143f068e
Dell <zsamora@ug.uchile.cl>
2019-09-26 23:50:58 -0300

    Inicio de archivos

---
+++ src/FirstAlgorithm.java
@@ -0,0 +1,5 @@
+public class FirstAlgorithm {
+    public int calculateDistance(int N, int B, String DIR_1, String DIR_2) {
+        return 0;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,25 @@ import java.io.File;
 
 public class Main {
 
-    public final static int MAX_M = 160;
-    public final static int MIN_M = (int) (MAX_M * 0.4);
-    public static int DISK_ACCESSES = 0;
-    public static final String DIR = "data" + File.separator;
-    public static final File FILE = new File(DIR + "id");
+    // Parámetros
+    public final static int M = 160;
     private static final int N = (int) Math.pow(2, 14);
+    public static int DISK_ACCESSES = 0;
+    // Strings en memoria externa
+    public static final String DIR_1 = "data" + File.separator;
+    public static final String DIR_2 = "data" + File.separator;
+    public static final File FILE_1 = new File(DIR_1 + "id");
+    public static final File FILE_2 = new File(DIR_2 + "id");
 
     public static void main(String[] args) {
-
         long startTime;
         long endTime;
-        // Search
+        // Crear strings progresivamente e ir guardandolos en memoria
+        char x = 'x';
+        System.out.println(x);
+        //
         startTime = System.currentTimeMillis();
-        // Funcion
+        // Funcion aqui
         endTime = System.currentTimeMillis();
         System.out.println("Tiempo promedio: " + ((endTime - startTime) / (N / 10)) + " milisegundos");
         System.out.println("N° total de accesos a disco: " + DISK_ACCESSES);
