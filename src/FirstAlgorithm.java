5b8ec6f37c4966297c0707ec47e61ead542a3aef
Dell <zsamora@ug.uchile.cl>
2019-09-28 21:27:47 -0300

    Creación de strings aleatorios

--- src/FirstAlgorithm.java
+++ src/FirstAlgorithm.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 
 public class FirstAlgorithm {
@@ -9,11 +6,10 @@ public class FirstAlgorithm {
     int B;
     String X;
     String Y;
-    String IntegerList = "Integers.txt";
-    ArrayList<Character> arrayX;
-    ArrayList<Character> arrayY;
+    String IntegerList = "Integers.bin";
+    ArrayList<String> arrayX;
+    ArrayList<String> arrayY;
     ArrayList<Integer> arrayInts;
-    ArrayList<Integer> arrayIntsRes;
     int upLeft = 0;
     int downLeft = 1;
 
@@ -22,7 +18,7 @@ public class FirstAlgorithm {
         this.B = B;
     }
 
-    public int calculateDistance (String X, String Y, int N) throws IOException {
+    public void calculateDistance(String X, String Y, int N) throws IOException {
         this.X = X;
         this.Y = Y;
         int row = 0;
@@ -36,22 +32,21 @@ public class FirstAlgorithm {
             downLeft += 1;
         }
     }
-    public void readBlocks() throws IOException {
-        BufferedReader readerX = new BufferedReader(new FileReader(X));
+    public void readBlocks() throws IOException, ClassNotFoundException {
+        FileInputStream fisX = new FileInputStream(X);
+        ObjectInputStream oisX = new ObjectInputStream(fisX);
         BufferedReader readerInt = new BufferedReader(new FileReader(IntegerList));
-        char[] bufferX = new char[this.B];
         char[] bufferInt = new char[this.B];
         // M / 5 pues son 1 byte char + 4 byte int resultado = 5 bytes
         for (int i = 0; i < this.M / 5; i += this.B) {
-            readerX.read(bufferX, i, this.B);
-            for (char x : bufferX) {
-                arrayX.add(x);
-            }
-            readerInt.read(bufferInt, i, this.B);
+            System.out.println();
+            arrayX.add((String) oisX.readObject());
             for (char j : bufferInt) {
                 arrayInts.add(Character.getNumericValue(j));
             }
         }
+        oisX.close();
+        fisX.close();
     }
     public void readRows(int row) throws IOException {
         BufferedReader readerY = new BufferedReader(new FileReader(Y));
--- src/Main.java
+++ src/Main.java
@@ -5,39 +5,57 @@ public class Main {
 
     // Parámetros
     // -Xmx 1GB (maximo uso de RAM) -Xms 64MB (minimo uso de RAM)
-    private static final int N = (int) Math.pow(2, 14);
-    private static final int B = (int) Math.pow(2, 10);
-    public final static int M = 20*B;
+    private static final int N = (int) Math.pow(2, 5);
+    private static final int B = (int) Math.pow(2, 3);
+    public final static int M = 20 * B;
     public static int DISK_ACCESSES = 0;
     private static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     // Strings en memoria externa
-    public static final String X = "StringX.txt";
-    public static final String Y = "StringY.txt";
+    public static final String X = "StringX.bin";
+    public static final String Y = "StringY.bin";
     // Librerias
     private static Random random = new Random();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         // Medición de tiempo
         long startTime;
         long endTime;
         // String Builder para escribir los strings en memoria
         StringBuilder strBuilderX = new StringBuilder();
         StringBuilder strBuilderY = new StringBuilder();
+        // Output Stream para escribir strings
+        FileOutputStream fosX = new FileOutputStream(X);
+        ObjectOutputStream oosX = new ObjectOutputStream(fosX);
+        FileOutputStream fosY = new FileOutputStream(Y);
+        ObjectOutputStream oosY = new ObjectOutputStream(fosY);
         // Crear N caracteres y agregarlos al string builder
         for (int i = 0; i < N; i++) {
             strBuilderX.append(ALPHABET.charAt(random.nextInt(ALPHABET.length())));
             strBuilderY.append(ALPHABET.charAt(random.nextInt(ALPHABET.length())));
+            if ((i + 1) % B == 0 || i == N - 1) {
+                oosX.writeObject(strBuilderX.toString());
+                oosY.writeObject(strBuilderY.toString());
+                strBuilderX = new StringBuilder();
+                strBuilderY = new StringBuilder();
+            }
         }
-        try {
-            BufferedWriter writerX = new BufferedWriter(new FileWriter(X));
-            BufferedWriter writerY = new BufferedWriter(new FileWriter(Y));
-            writerX.write(strBuilderX.toString());
-            writerY.write(strBuilderY.toString());
-            writerX.close();
-            writerY.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        oosX.close();
+        oosY.close();
+        fosX.close();
+        fosY.close();
+        FileInputStream fisX = new FileInputStream(X);
+        ObjectInputStream oisX = new ObjectInputStream(fisX);
+        FileInputStream fisY = new FileInputStream(Y);
+        ObjectInputStream oisY = new ObjectInputStream(fisY);
+        // Leer X por bloques
+        String[] listX = new String[(int) Math.ceil((1.0 * N)/B)];
+        for(int j = 0; j < listX.length; j++) {
+            System.out.println(oisX.readObject());
         }
+        oisX.close();
+        oisY.close();
+        fisX.close();
+        fisY.close();
         // Warm up
         // Funcion aqui
 
