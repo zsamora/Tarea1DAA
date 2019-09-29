aa96cc01ba5687e35ae12976f474530b8c36ad4d
Dell <zsamora@ug.uchile.cl>
2019-09-29 19:23:29 -0300

    Version casi casi

--- src/FirstAlgorithm.java
+++ src/FirstAlgorithm.java
@@ -4,33 +4,36 @@ import java.util.ArrayList;
 public class FirstAlgorithm {
     int M;
     int B;
+    int upLeft;
+    int downLeft;
+    int upLeftTemp;
+    int downLeftTemp;
     String X;
     String Y;
     String IntegerList;
-    ArrayList<String> arrayX;
-    ArrayList<String> arrayY;
+    String subX;
+    String subY;
     ArrayList<Integer> arrayInts;
     ArrayList<Integer> arrayIntsTemp;
-    String IntegerListTemp = "IntegersTemp.bin";
 
     public FirstAlgorithm(int M, int B) {
         this.M = M;
         this.B = B;
+        upLeft = 0;
+        downLeft = 1;
     }
 
-    public void calculateDistance(String X, String Y, String IntegerList, int N) throws IOException, ClassNotFoundException {
+    public int calculateDistance(String X, String Y, String IntegerList, int N) throws IOException, ClassNotFoundException {
         this.X = X;
         this.Y = Y;
         this.IntegerList = IntegerList;
         int row = 0;
         int col = 0;
-        int upLeft = 0;
-        int downLeft = 1;
+        int result = -1;
         while (row < N) {
-            // Leemos un bloque del segundo string si pasamos B filas (caracteres)
+            // Leemos un bloque del segundo string (vertical) si pasamos B filas (caracteres)
             if (row % this.B == 0)
-                readRows(row);
-            System.out.println(arrayY);
+                readRows();
             // Abrimos los Stream para lectura de archivos del primer string y la fila de enteros (previa y temporal)
             FileInputStream fisX = new FileInputStream(X);
             ObjectInputStream oisX = new ObjectInputStream(fisX);
@@ -38,65 +41,82 @@ public class FirstAlgorithm {
             ObjectInputStream oisInt = new ObjectInputStream(fisInt);
             // Array para guardar todos los enteros calculados en la fila
             this.arrayIntsTemp = new ArrayList<Integer>();
-            // Leemos el segundo string de un bloque por iteración (N/B veces)
+            // Seteamos los valores upLeftTemp y downLeftTemp como los de la primera columna
+            this.upLeftTemp = upLeft;
+            this.downLeftTemp = downLeft;
+            // Leemos el primer string (horizontal) de un bloque por iteración (N/B veces)
             while (col < N / this.B) {
                 // Reiniciamos el array del primer string y los valores calculados en el array de enteros
-                this.arrayX = new ArrayList<String>();
                 this.arrayInts = new ArrayList<Integer>();
                 // Llenamos la memoria solo con un bloque de String, que tiene 4 bloques de Integer asociado
-                this.arrayX.add((String) oisX.readObject());
+                this.subX = (String) oisX.readObject();
                 for (int j = 0; j < 4 ; j++)
                     this.arrayInts.addAll((ArrayList<Integer>) oisInt.readObject());
-                calculateRow(upLeft, downLeft, row);
-                writeRow();
+                System.out.println("---- Int ----");
+                System.out.println(arrayInts);
+                System.out.println("---- subX ----");
+                System.out.println(subX);
+                // Calculamos la fila
+                calculateRow(row % this.B);
+                System.out.println("---- TempInt ----");
+                System.out.println(arrayIntsTemp);
+                col += 1;
             }
-            row += 1;
-            upLeft += 1;
-            downLeft += 1;
-            oisX.close();
-            fisX.close();
             oisInt.close();
             fisInt.close();
+            oisX.close();
+            fisX.close();
+            // Escribimos todos los enteros
+            result = writeRow();
+            row += 1;
+            this.upLeft += 1;
+            this.downLeft += 1;
+            col = 0;
         }
+        return result;
     }
-    public void readRows(int row) throws IOException, ClassNotFoundException {
-        this.arrayY = new ArrayList<String>();
+    public void readRows() throws IOException, ClassNotFoundException {
         FileInputStream fisY = new FileInputStream(Y);
         ObjectInputStream oisY = new ObjectInputStream(fisY);
-        this.arrayY.add((String) oisY.readObject());
+        this.subY = (String) oisY.readObject();
     }
 
-    public void calculateRow(int upLeft, int downLeft, int row) throws IOException {
+    public void calculateRow(int row) throws IOException {
         int newDownLeft;
+        int up;
+        System.out.println("---- charY ----");
+        System.out.println(subY.charAt(row));
         for (int i = 0; i < this.arrayInts.size(); i ++) {
-            if (arrayX.get(i % 4) == arrayY.get(row)) {
-                newDownLeft = Math.min(upLeft, Math.min(downLeft + 1, arrayInts.get(i) + 1));
+            up = arrayInts.get(i);
+            if (subX.charAt(i % 4) == subY.charAt(row)) {
+                newDownLeft = Math.min(this.upLeftTemp, Math.min(this.downLeftTemp + 1, up + 1));
                 this.arrayIntsTemp.add(newDownLeft);
-                downLeft = newDownLeft;
+                this.downLeftTemp = newDownLeft;
             }
             else {
-                newDownLeft = Math.min(upLeft + 1, Math.min(downLeft + 1, arrayInts.get(i) + 1));
+                newDownLeft = Math.min(this.upLeftTemp + 1, Math.min(this.downLeftTemp + 1, up + 1));
                 this.arrayIntsTemp.add(newDownLeft);
-                downLeft = newDownLeft;
+                this.downLeftTemp = newDownLeft;
             }
+            this.upLeftTemp = up;
         }
-        return newDownleft;
     }
 
-    public void writeRow() throws IOException {
+    public int writeRow() throws IOException {
         // Array List para escribir los enteros
         ArrayList<Integer> intTempBuilder = new ArrayList<Integer>();
         // Output Stream para escribir enteros temporales
-        FileOutputStream fosI = new FileOutputStream(IntegerListTemp);
+        FileOutputStream fosI = new FileOutputStream(IntegerList);
         ObjectOutputStream oosI = new ObjectOutputStream(fosI);
         for (int i = 0; i < arrayIntsTemp.size(); i++) {
+            intTempBuilder.add(arrayIntsTemp.get(i));
             if ((i + 1) % (B / 4) == 0) {
-                System.out.println(intTempBuilder);
                 oosI.writeObject(intTempBuilder);
                 intTempBuilder = new ArrayList<Integer>();
             }
         }
         fosI.close();
         oosI.close();
+        return arrayIntsTemp.get(arrayIntsTemp.size()-1);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,11 @@ public class Main {
 
     // Parámetros
     // -Xmx 1GB (maximo uso de RAM) -Xms 64MB (minimo uso de RAM)
-    private static final int N = (int) Math.pow(2, 5);
-    private static final int B = (int) Math.pow(2, 3);
+    private static final int N = (int) Math.pow(2, 3);
+    private static final int B = (int) Math.pow(2, 2);
     public final static int M = 20 * B;
     public static int DISK_ACCESSES = 0;
-    private static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final String ALPHABET = "AB";//CDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     // Strings en memoria externa
     public static final String X = "StringX.bin";
     public static final String Y = "StringY.bin";
@@ -56,36 +56,44 @@ public class Main {
         fosX.close();
         fosY.close();
         // Leer X por bloques
-        /*FileInputStream fisX = new FileInputStream(X);
+        FileInputStream fisX = new FileInputStream(X);
         ObjectInputStream oisX = new ObjectInputStream(fisX);
+        System.out.println("----- X -----");
         for(int j = 0; j < (int) Math.ceil((1.0 * N)/B); j++) {
-            System.out.println(oisX.readObject());
+            System.out.print(oisX.readObject());
         }
+        System.out.println();
         oisX.close();
-        fisX.close();*/
+        fisX.close();
         // Leer Y por bloques
-        /*FileInputStream fisY = new FileInputStream(Y);
+        FileInputStream fisY = new FileInputStream(Y);
         ObjectInputStream oisY = new ObjectInputStream(fisY);
+        System.out.println("----- Y -----");
         for(int j = 0; j < (int) Math.ceil((1.0 * N)/B); j++) {
-            System.out.println(oisY.readObject());
+            System.out.print(oisY.readObject());
         }
+        System.out.println();
         oisY.close();
-        fisY.close();*/
+        fisY.close();
+        // Warm up
+        FirstAlgorithm firstAlgorithm = new FirstAlgorithm(M, B);
+        //firstAlgorithm.calculateDistance(X, Y, IntegerList, N);
+        // Prueba real
+        startTime = System.currentTimeMillis();
+        int distance = firstAlgorithm.calculateDistance(X, Y, IntegerList, N);
+        endTime = System.currentTimeMillis();
+        System.out.println();
+        System.out.print("La distancia es: ");
+        System.out.println(distance);
         // Leer la lista de enteros por bloque (menos enteros pues son mas pesados)
         /*FileInputStream fisInt = new FileInputStream(IntegerList);
         ObjectInputStream oisInt = new ObjectInputStream(fisInt);
+        System.out.println("---- Ints ----");
         for(int j = 0; j < (int) Math.ceil((1.0 * N) / (B / 4)); j++) {
             System.out.println(oisInt.readObject());
         }
         oisInt.close();
         fisInt.close();*/
-        // Warm up
-        FirstAlgorithm firstAlgorithm = new FirstAlgorithm(M, B);
-        firstAlgorithm.calculateDistance(X, Y, IntegerList, N);
-        // Prueba real
-        startTime = System.currentTimeMillis();
-        //firstAlgorithm.calculateDistance(X, Y, IntegerList, N);
-        endTime = System.currentTimeMillis();
         System.out.println("Tiempo total: " + (endTime - startTime) + " milisegundos");
         System.out.println("N° total de accesos a disco: " + DISK_ACCESSES);
     }
